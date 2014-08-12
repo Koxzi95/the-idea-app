09c2a10b17744cf8ec23f41416b94d19ff25166c
Tom Cox <tom.jcox@icloud.com>
2014-08-12 03:43:04 +0100

    Added user authentication and login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ TheIdeaApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #This is for devise to use the localhost
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
