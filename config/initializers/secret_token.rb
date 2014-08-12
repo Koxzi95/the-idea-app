2a86210c6ea5437310366cd86c1f15667f91633f
Tom Cox <tom.jcox@icloud.com>
2014-08-12 02:22:40 +0100

    Init setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheIdeaApp::Application.config.secret_key_base = 'eac4d5e5b4fc890e8989b4ef3f1503028a71bec69e43052c795a45241877a10f56c35f7552a10a5b2aecd28f1781ef811292d0e3c1cc5a4cb46cb54ca540738e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheIdeaApp::Application.config.session_store :cookie_store, key: '_the-idea-app_session'
